892c0873d62138ad3a9658210c825599da884c32
louishong1127 <ts00100665@gmail.com>
2019-08-28 16:30:33 +0900

    [python] Decode String.

---
+++ python/DecodeString.py
@@ -0,0 +1,25 @@
+class Solution(object):
+    def decodeString(self, s):
+        """
+        :type s: str
+        :rtype: str
+        """
+        stack = []; 
+        curNum = 0; 
+        curString = ''
+        for c in s:
+            if c == '[':
+                stack.append(curString)
+                stack.append(curNum)
+                curString = ''
+                curNum = 0
+            elif c == ']':
+                num = stack.pop()
+                prevString = stack.pop()
+                curString = prevString + num*curString
+            elif c.isdigit():
+                curNum = curNum*10 + int(c)
+            else:
+                curString += c
+        return curString
+        
